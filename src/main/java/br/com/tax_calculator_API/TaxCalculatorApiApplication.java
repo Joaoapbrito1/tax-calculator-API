0153186c17af2e8209173360a6310b0554139908
Joaoapbrito1 <joao.brito@zup.com.br>
2025-02-28 17:05:32 -0300

    chore: initial project setup.

---
+++ src/main/java/br/com/tax_calculator_API/TaxCalculatorApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.tax_calculator_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxCalculatorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxCalculatorApiApplication.class, args);
+	}
+
+}
