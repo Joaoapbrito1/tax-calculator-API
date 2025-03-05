436bb381c405b713d7a0d0854db6b59a0d0ec43d
Joaoapbrito1 <joao.brito@zup.com.br>
2025-03-05 16:04:27 -0300

    feat: implements the RoleModel and UserModel classes.

---
+++ src/main/java/br/com/tax_calculator_API/models/UserModel.java
@@ -0,0 +1,30 @@
+package br.com.tax_calculator_API.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Set;
+
+@Entity
+@Data
+@Table(name = "users")
+public class UserModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
+    )
+    private Set<RoleModel> roleModels;
+    public UserModel() {
+
+    }
+}
