b3df36355d0bfa3c953b53e41f0cb8d5e7de179f
Joaoapbrito1 <joao.brito@zup.com.br>
2025-03-11 15:23:18 -0300

    feat: Implements the AuthService interface and the AuthServiceImpl class.

--- src/main/java/br/com/tax_calculator_API/services/AuthService.java
+++ src/main/java/br/com/tax_calculator_API/services/AuthService.java
@@ -1,6 +1,10 @@
 package br.com.tax_calculator_API.services;
 
 
+import br.com.tax_calculator_API.dtos.JwtResponseDTO;
+import br.com.tax_calculator_API.dtos.UserLoginDTO;
+
 public interface AuthService {
+    JwtResponseDTO login(UserLoginDTO userLoginDTO);
 
 }
