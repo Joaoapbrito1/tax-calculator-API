ba5520e68ade6a056805176de15a26d034bac65b
Joaoapbrito1 <joao.brito@zup.com.br>
2025-03-11 14:58:30 -0300

    feat: Creates the AuthService interface and the AuthServiceImpl class.

---
+++ src/main/java/br/com/tax_calculator_API/services/AuthService.java
@@ -0,0 +1,6 @@
+package br.com.tax_calculator_API.services;
+
+
+public interface AuthService {
+
+}
---
+++ src/main/java/br/com/tax_calculator_API/services/AuthServiceImpl.java
@@ -0,0 +1,11 @@
+package br.com.tax_calculator_API.services;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthServiceImpl implements AuthService {
+
+}
