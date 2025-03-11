f831e2d9ce4de5a19d25aac9f2d587e309bc84ad
Joaoapbrito1 <joao.brito@zup.com.br>
2025-03-11 14:11:14 -0300

    feat: insert annotation to have an empty constructor.

--- src/main/java/br/com/tax_calculator_API/dtos/UserLoginDTO.java
+++ src/main/java/br/com/tax_calculator_API/dtos/UserLoginDTO.java
@@ -1,10 +1,10 @@
 package br.com.tax_calculator_API.dtos;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Getter
-@AllArgsConstructor
+@Data
+@NoArgsConstructor
 public class UserLoginDTO {
     private String username;
     private String password;
