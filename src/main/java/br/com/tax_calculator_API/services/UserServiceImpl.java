584b0dfad4ef10a6a57d9cada885d0b3563a1399
Joaoapbrito1 <joao.brito@zup.com.br>
2025-03-12 15:47:16 -0300

    feat: implements the UserServiceImpl class and the UserService interface.

---
+++ src/main/java/br/com/tax_calculator_API/services/UserServiceImpl.java
@@ -0,0 +1,39 @@
+package br.com.tax_calculator_API.services;
+
+
+import br.com.tax_calculator_API.dtos.UserRequestDTO;
+import br.com.tax_calculator_API.models.UserModel;
+import br.com.tax_calculator_API.models.UserRoleModel;
+import br.com.tax_calculator_API.repository.UserRepository;
+import br.com.tax_calculator_API.repository.UserRoleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl{
+
+    private final UserRepository userRepository;
+    private final UserRoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public void registerUser(UserRequestDTO userRequestDTO) {
+        if (userRepository.existsByUsername(userRequestDTO.getUsername())) {
+            throw new RuntimeException("Unprocess Entity");
+        }
+
+        UserModel user = new UserModel();
+        user.setUsername(userRequestDTO.getUsername());
+        user.setPassword(passwordEncoder.encode(userRequestDTO.getPassword()));
+
+        Set<UserRoleModel> roles = userRequestDTO.getRoles().stream()
+                .map(r -> new UserRoleModel(r.name()))
+                .collect(Collectors.toSet());
+        roleRepository.saveAll(roles);
+        user.setUserRoleModels(roles);
+        userRepository.save(user);
+    }
+}
