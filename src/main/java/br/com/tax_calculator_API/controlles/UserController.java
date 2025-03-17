4d915454209a7248ea1d6d241e8432660ec60037
Joaoapbrito1 <joao.brito@zup.com.br>
2025-03-17 14:39:51 -0300

    feat: Add Swagger annotations to the UserController class for documentation generation.

--- src/main/java/br/com/tax_calculator_API/controlles/UserController.java
+++ src/main/java/br/com/tax_calculator_API/controlles/UserController.java
@@ -2,35 +2,65 @@ package br.com.tax_calculator_API.controlles;
 
 import br.com.tax_calculator_API.dtos.JwtResponseDTO;
 import br.com.tax_calculator_API.dtos.UserLoginDTO;
-import br.com.tax_calculator_API.services.AuthService;
 import br.com.tax_calculator_API.dtos.UserRequestDTO;
-import br.com.tax_calculator_API.services.UserServiceImpl;
+import br.com.tax_calculator_API.exeptions.UserAlreadyExistsException;
+import br.com.tax_calculator_API.infra.swegger.SwaggerConfig;
+import br.com.tax_calculator_API.services.AuthService;
+import br.com.tax_calculator_API.services.impl.UserServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/api/user")
 @RequiredArgsConstructor
+@Tag(name = "Usuarios", description = "Controle de usuarios")
+@SecurityRequirement(name = SwaggerConfig.SECURITY)
 public class UserController {
 
-    @Autowired
-    private UserServiceImpl userServiceImpl;
+    private final UserServiceImpl userServiceImpl;
+    private final AuthService authService;
 
-    @Autowired
-    private AuthService authService;
-
-    @PostMapping ("/register")
-    public void registerUser(@RequestBody UserRequestDTO userRequestDTO){
-        userServiceImpl.registerUser(userRequestDTO);
+    @PostMapping("/register")
+    @Operation(
+            summary = "Registra usuario.",
+            description = "Método para cadastrar um usuario",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Usuario cadastrado com sucesso."),
+                    @ApiResponse(responseCode = "400", description = "Usuario ja cadastrado.", content = @Content),
+                    @ApiResponse(responseCode = "500", description = "Erro interno no servidor.", content = @Content)
+            }
+    )
+    public ResponseEntity<String> registerUser(@RequestBody UserRequestDTO userRequestDTO) {
+        try {
+            userServiceImpl.registerUser(userRequestDTO);
+            return ResponseEntity.status(HttpStatus.CREATED).body("Usuario cadastrado com sucesso.");
+        } catch (UserAlreadyExistsException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuario ja cadastrado.");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao cadastrar usuario.");
+        }
     }
 
-    @PostMapping ("/login")
-    public ResponseEntity<JwtResponseDTO> authenticateUser(@RequestBody UserLoginDTO userLoginDTO) {
-        return ResponseEntity.ok(authService.login(userLoginDTO));
+    @PostMapping("/login")
+    @Operation(
+            summary = "Login usuario.",
+            description = "Método para o usuario logar.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Usuario logado com sucesso.",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = JwtResponseDTO.class))),
+                    @ApiResponse(responseCode = "400", description = "Esse usuario não existe.", content = @Content)
+            }
+    )
+    public ResponseEntity<JwtResponseDTO> login(@RequestBody UserLoginDTO userLoginDTO) {
+        JwtResponseDTO jwtResponseDTO = authService.login(userLoginDTO);
+        return new ResponseEntity<>(jwtResponseDTO, HttpStatus.OK);
     }
 }
