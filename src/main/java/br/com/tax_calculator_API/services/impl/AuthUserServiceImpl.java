8c61e3e49afb0f015094b7eee45f3fe81b22b874
Joaoapbrito1 <joao.brito@zup.com.br>
2025-03-17 14:30:02 -0300

    feat: Edit the AuthUserServiceImpl class and add the deleteResource method.

--- src/main/java/br/com/tax_calculator_API/services/impl/AuthUserServiceImpl.java
+++ src/main/java/br/com/tax_calculator_API/services/impl/AuthUserServiceImpl.java
@@ -1,43 +1,45 @@
-package br.com.tax_calculator_API.services;
+package br.com.tax_calculator_API.services.impl;
 
 import br.com.tax_calculator_API.dtos.UserResponseDTO;
+import br.com.tax_calculator_API.exeptions.InvalidDataException;
+import br.com.tax_calculator_API.exeptions.ResourceNotFoundException;
+import br.com.tax_calculator_API.exeptions.UserNotAuthenticatedException;
 import br.com.tax_calculator_API.infra.jwt.JwtTokenProvider;
-import br.com.tax_calculator_API.services.impl.AuthUserService;
+import br.com.tax_calculator_API.services.AuthUserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @RequiredArgsConstructor
 public class AuthUserServiceImpl implements AuthUserService {
-
     private final JwtTokenProvider jwtTokenProvider;
 
     @Override
-    public UserResponseDTO getUserInfo(String token) {
+    public UserResponseDTO getUserInfo(String token) throws UserNotAuthenticatedException {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
         if (authentication == null || !authentication.isAuthenticated()) {
-            throw new RuntimeException("Usuário não autenticado");
+            throw new UserNotAuthenticatedException("Usuário não autenticado");
         }
-
         String username = authentication.getName();
-
         return new UserResponseDTO("Bem-vindo, " + username + "!");
     }
 
     @Override
     public String adminAccess() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
         if (authentication.getAuthorities().stream()
                 .anyMatch(authority -> authority.getAuthority().equals("ROLE_USER"))) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Acesso negado para usuários com o papel ROLE_USER");
+            throw new InvalidDataException("Acesso negado para usuários com o papel ROLE_USER");
         }
-
         return "Acesso admin";
+    }
+
+    public void deleteResource(Long resourceId) {
+        boolean resourceExists = false;
+        if (!resourceExists) {
+            throw new ResourceNotFoundException("Recurso não encontrado");
+        }
     }
 }
