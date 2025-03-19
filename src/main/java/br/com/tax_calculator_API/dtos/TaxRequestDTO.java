73cec9d383e1f89d863eb2b6bf88a379f37098ab
Joaoapbrito1 <joao.brito@zup.com.br>
2025-03-19 14:45:56 -0300

    feat: Add constructors.

--- src/main/java/br/com/tax_calculator_API/dtos/TaxRequestDTO.java
+++ src/main/java/br/com/tax_calculator_API/dtos/TaxRequestDTO.java
@@ -1,10 +1,14 @@
 package br.com.tax_calculator_API.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class TaxRequestDTO {
    private String name;
    private String description;
