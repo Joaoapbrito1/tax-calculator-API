c5c7d4067ad3e5a6c75512c711032f26917b4857
Joaoapbrito1 <joao.brito@zup.com.br>
2025-03-19 14:46:39 -0300

    feat: Add constructor.

--- src/main/java/br/com/tax_calculator_API/dtos/UserLoginDTO.java
+++ src/main/java/br/com/tax_calculator_API/dtos/UserLoginDTO.java
@@ -1,10 +1,12 @@
 package br.com.tax_calculator_API.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class UserLoginDTO {
     private String username;
     private String password;
