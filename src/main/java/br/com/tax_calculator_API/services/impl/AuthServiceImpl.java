db329d9ef382464b76fcd817f874f82e981b41d9
Joaoapbrito1 <joao.brito@zup.com.br>
2025-03-19 14:59:11 -0300

    feat: Add exception handling in AuthServiceImpl class.

--- src/main/java/br/com/tax_calculator_API/services/impl/AuthServiceImpl.java
+++ src/main/java/br/com/tax_calculator_API/services/impl/AuthServiceImpl.java
@@ -6,11 +6,15 @@ import br.com.tax_calculator_API.infra.jwt.JwtTokenProvider;
 import br.com.tax_calculator_API.repository.UserRepository;
 import br.com.tax_calculator_API.services.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @RequiredArgsConstructor
@@ -22,17 +26,23 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     public JwtResponseDTO login(UserLoginDTO userLoginDTO) {
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        userLoginDTO.getUsername(),
-                        userLoginDTO.getPassword()
-                )
-        );
+        try {
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            userLoginDTO.getUsername(),
+                            userLoginDTO.getPassword()
+                    )
+            );
 
-        SecurityContextHolder.getContext().setAuthentication(authentication);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
 
-        String token = jwtTokenProvider.generateToken(authentication);
+            String token = jwtTokenProvider.generateToken(authentication);
 
-        return new JwtResponseDTO(token);
+            return new JwtResponseDTO(token);
+        } catch (UsernameNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User not found.");
+        } catch (BadCredentialsException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Incorrect password.");
+        }
     }
 }
