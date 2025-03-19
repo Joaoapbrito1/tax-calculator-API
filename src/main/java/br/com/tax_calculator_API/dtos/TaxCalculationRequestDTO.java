03fc15e957a45ef911667f8f2ab19bf211b49c7a
Joaoapbrito1 <joao.brito@zup.com.br>
2025-03-19 14:45:09 -0300

    feat: Add empty constructor.

--- src/main/java/br/com/tax_calculator_API/dtos/TaxCalculationRequestDTO.java
+++ src/main/java/br/com/tax_calculator_API/dtos/TaxCalculationRequestDTO.java
@@ -2,11 +2,13 @@ package br.com.tax_calculator_API.dtos;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class TaxCalculationRequestDTO {
     private Long taxTypeId;
     private Double valueBase;
